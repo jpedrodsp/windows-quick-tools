124057cf6cfc8493a8bbeba64768ecb2a1daf045
João Pedro Patrocinio <jpedro9966@gmail.com>
2020-01-12 17:49:21 -0300

    Added 7-Zip installation script on First Setup

--- firstsetup/firstsetup.bat
+++ firstsetup/firstsetup.bat
@@ -4,20 +4,24 @@ start %~dp0\scripts\chocolatey-cmd-install.bat
 
 ::Install Google Chrome
 echo "Installing Google Chrome"
-start %~dp0\scripts\install_google-chrome.bat
+start /wait %~dp0\scripts\install_google-chrome.bat
 
 ::Install Visual Studio Code and Git
 echo "Installing Visual Studio Code and Git"
-start %~dp0\scripts\install_code-and-git.bat
+start /wait %~dp0\scripts\install_code-and-git.bat
 
 ::Install QBitTorrent"
 echo "Installing QBitTorrent"
-start %~dp0\scripts\install_qbittorrent.bat
+start /wait %~dp0\scripts\install_qbittorrent.bat
 
 ::Install Python 3
 echo "Installing Python 3"
-start %~dp0\scripts\install_python3.bat
+start /wait %~dp0\scripts\install_python3.bat
 
 ::Install Youtube-dl
 echo "Installing Youtube-dl"
-start %~dp0\scripts\install_youtube-dl.bat
+start /wait %~dp0\scripts\install_youtube-dl.bat
+
+::Install 7-Zip
+echo "Installing 7-Zip"
+start /wait %~dp0\scripts\install_7zip.bat
---
+++ firstsetup/scripts/install_7zip.bat
@@ -0,0 +1,6 @@
+::echo off
+
+SET DIR=%~dp0%
+
+::[Install] 7-Zip
+choco install -y 7zip
