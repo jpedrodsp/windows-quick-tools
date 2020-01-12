b37888ebfeef1d391478bd9fd7b599c623f4bc01
João Pedro Patrocinio <jpedro9966@gmail.com>
2020-01-12 18:04:06 -0300

    Sequential program upgrade

--- firstsetup/firstsetup.bat
+++ firstsetup/firstsetup.bat
@@ -1,27 +1,31 @@
 ::echo off
 echo "Changing to directory: %~dp0"
-start %~dp0\scripts\chocolatey-cmd-install.bat
+SET DIR=%~dp0%
+
+::Install Chocolatey
+echo "Installing Chocolatey"
+start "Please wait..." /wait "cmd.exe" "/c %~dp0scripts\chocolatey-cmd-install.bat"
 
 ::Install Google Chrome
 echo "Installing Google Chrome"
-start /wait %~dp0\scripts\install_google-chrome.bat
+start "Please wait..." /wait "cmd.exe" "/c %~dp0scripts\install_google-chrome.bat"
 
 ::Install Visual Studio Code and Git
 echo "Installing Visual Studio Code and Git"
-start /wait %~dp0\scripts\install_code-and-git.bat
+start "Please wait..." /wait "cmd.exe" "/c %~dp0scripts\install_code-and-git.bat"
 
 ::Install QBitTorrent"
 echo "Installing QBitTorrent"
-start /wait %~dp0\scripts\install_qbittorrent.bat
+start "Please wait..." /wait "cmd.exe" "/c %~dp0scripts\install_qbittorrent.bat"
 
 ::Install Python 3
 echo "Installing Python 3"
-start /wait %~dp0\scripts\install_python3.bat
+start "Please wait..." /wait "cmd.exe" "/c %~dp0scripts\install_python3.bat"
 
 ::Install Youtube-dl
 echo "Installing Youtube-dl"
-start /wait %~dp0\scripts\install_youtube-dl.bat
+start "Please wait..." /wait "cmd.exe" "/c %~dp0scripts\install_youtube-dl.bat"
 
 ::Install 7-Zip
 echo "Installing 7-Zip"
-start /wait %~dp0\scripts\install_7zip.bat
+start "Please wait..." /wait "cmd.exe" "/c %~dp0scripts\install_7zip.bat"
